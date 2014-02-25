d2e1cc59bbac7f849cb04dc038ffc07d963d80f1
Alejandro Baez <Alejan.Baez@gmail.com>
2014-02-25 14:27:41 -0500

    Moved key mapping to commons
    Just so things are much more managable, all the commons keymapping
    has been moved to the `init.lua` file.

--- init.lua
+++ init.lua
@@ -14,4 +14,20 @@ for filename in lfs.dir(_USERHOME..'/modules/common/') do
 end
 
 
+if not CURSES then
+  -- changes the theme for every hour.
+  M.themer.change()
+end
+
+events.connect(events.INITIALIZED, function()
+  -- deletes a line ore a selection.
+  keys['cK'] = {M.delete_line}
+  -- opens a terminal in the locaton
+  keys['cT'] = {M.open_terminal}
+  -- opens the `~/.textadept/init.lua` file
+  keys['cae'] = {M.quick_edit}
+  -- selects all similar words and puts a cursor on it!
+  keys["cG"] = {M.multiedit.select_all}
+end)
+
 return M
