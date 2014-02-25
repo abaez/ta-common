ccc7479b4b05993e687a532c1d2ea33b69124026
Alejandro Baez <Alejan.Baez@gmail.com>
2014-02-25 16:34:22 -0500

    first of the documentations

--- themer.lua
+++ themer.lua
@@ -1,5 +1,15 @@
--- theme changer
+--- the Textadept theme changer.
+-- See @{README.md} for details on usage.
+-- @author Alejandro Baez <alejan.baez@gmail.com>
+-- @copyright 2014
+-- @license MIT (see LICENSE)
+-- @module themer
 
+
+--- themes to choose from.
+-- A simple list to locate themes to apply. Follows the 8 hour work schedule
+-- for the brightness of the theme.
+-- @table by_17
 local by_17 = {
   "solarized",
   "atelierheath",
@@ -11,6 +21,10 @@ local by_17 = {
   "atelierdune",
 }
 
+
+--- picks the theme by hour.
+-- @function pick
+-- @return a table that holds theme type(light, dark) and the theme.
 local function pick()
   local hour = os.date("*t")["hour"]
 
@@ -20,21 +34,21 @@ local function pick()
   else
     background = "-dark"
   end
-  -- note: need to use
+  -- note: need to use for the refresh.
   return {background, hour % #by_17 + 1}
 end
 
-
+--- changes the theme according to pick.
+-- @function change
 local function change()
   ui.set_theme(
---  'base16-'.. by_17[1] .. "-dark",
   'base16-'.. by_17[pick()[2]] .. pick()[1],
   {font = "Inconsolata",
   fontsize = 14}
   )
 end
 
-
+--- @export
 return {
   change = change,
   pick = pick
