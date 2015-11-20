8a46c0900a5bb8c953e1a5a0351317f54dcee01e
Alejandro Baez <code@baez.nyc>
2015-11-19 21:16:02 -0500

    updated multiedit

--- multiedit.lua
+++ multiedit.lua
@@ -1,97 +1,290 @@
--- Keyboard short cuts for adding multiple cursors.<br>
--- Copyright (c) 2010 [Brian Schott (Sir Alaran)](http://caladbolg.net/textadeptwiki/index.php?n=Main.Multiedit).<br>
--- License: [MIT](http://www.opensource.org/licenses/mit-license.php).
+--------------------------------------------------------------------------------
+-- The MIT License
 --
--- The following buffer settings are required:
---     buffer.multiple_selection = true
---     buffer.additional_selection_typing = true
---     buffer.additional_carets_visible = true
+-- Copyright (c) 2015 Sebastian Neusser
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+-- THE SOFTWARE.
+--------------------------------------------------------------------------------
+
+
+local print = function(...)
+  local tmp = ui.SILENT_PRINT
+  ui.SILENT_PRINT = true
+  ui.print(...)
+  ui.SILENT_PRINT = tmp
+end
+
+local keys = _G.keys
+local editing = _G.textadept.editing
+
+-- no clue how to fix the weird multi_paste behavoiour yet :>
+--buffer.multi_paste = buffer.MULTIPASTE_EACH
+
+
 local M = {}
 
-local findall = require 'common.findall'
+-- only navigate in the current line
+M.STAY_IN_LINE = true
 
--- ## Setup
+local function get_new_position(olpos, inc)
 
-local positions = {}
-local restore = false
+  local nupos = buffer:position_relative(olpos, inc)
 
--- ## Commands
+  if M.STAY_IN_LINE then
+    local start = buffer:position_from_line(buffer:line_from_position(olpos))
+    local stop = buffer.line_end_position[buffer:line_from_position(olpos)]
 
--- Adds a single mark.
-function M.add_position()
-  table.insert(positions, buffer.current_pos)
-  buffer:add_selection(buffer.current_pos, buffer.current_pos)
-  restore = true
-end
-
--- Resets the cursor positions according to the positions table. This function
--- exists because Scintilla is grouchy and likes to kill the multi-selection.
-local function set_cursor_positions()
-  local prev_pos = buffer.current_pos
-  for key, pos in ipairs(positions) do
-    if pos ~= prev_pos then
-      if key == 1 then
-        buffer:clear_selections()
-        buffer:set_selection(pos, pos)
-      else
-        buffer:add_selection(pos, pos, 0)
-      end
+    if nupos < start or nupos > stop then
+      return olpos
+    else
+      return nupos
     end
-  end
-  if buffer.current_pos ~= prev_pos then
-    buffer:add_selection(prev_pos, prev_pos, 0)
+  else
+    return nupos
   end
 end
 
--- Multi-select all occurences of the word at the cursor position. This acts as
--- a very fast find-replace function. Use with caution, as this selects all
--- occurences of the word at the cursor.
-function M.select_all()
-  local start_position = buffer.current_pos
-  local occurences = findall.find_all_at_cursor()
-  local main_sel = 0
-  if #occurences > 1 then
-    for i, j in ipairs(occurences) do
-      if j[1] > start_position or j[2] < start_position then
-        buffer:add_selection(j[1], j[2])
-      else
-        main_sel = i
-      end
+local function get_next_pos(olpos)
+  return get_new_position(olpos, 1)
+end
+
+local function get_prev_pos(olpos)
+  return get_new_position(olpos, -1)
+end
+
+M.select_word = function(prevent_scrolling)
+
+  local word = buffer:text_range(buffer.selection_start, buffer.selection_end)
+
+  if word == '' then
+    buffer:set_selection(
+      buffer:word_start_position(buffer.current_pos, true),
+      buffer:word_end_position(buffer.current_pos, true))
+    return not buffer.selection_empty
+  end
+
+  buffer.search_flags = buffer.FIND_MATCHCASE
+  if buffer.selection_n_end[buffer.selections - 1] > buffer.selection_n_start[buffer.main_selection] then
+    buffer:set_target_range(buffer.selection_n_end[buffer.selections - 1], buffer.length - 1)
+    -- wrap around if not found
+    if buffer:search_in_target(word) == -1 then
+      buffer:set_target_range(0, buffer.selection_n_start[buffer.main_selection])
     end
-    buffer:add_selection(occurences[main_sel][1], occurences[main_sel][2])
-    while buffer.selection_start > start_position
-        or buffer.selection_end < start_position do
-      buffer:rotate_selection()
+  else
+    buffer:set_target_range(buffer.selection_n_end[buffer.selections - 1], buffer.selection_n_start[buffer.main_selection])
+  end
+
+  if buffer:search_in_target(word) ~= -1 then
+    buffer:add_selection(buffer.target_start, buffer.target_end)
+    buffer.main_selection = 0
+
+    if buffer.selection_n_caret[buffer.main_selection] == buffer.selection_n_end[buffer.main_selection] then
+      buffer.selection_n_caret[buffer.selections - 1] = buffer.target_end
+      buffer.selection_n_anchor[buffer.selections - 1] =  buffer.target_start
     end
-  elseif #occurences == 1 then
-    buffer:set_selection(occurences[1][1], occurences[1][2])
+
+    if not prevent_scrolling then
+      buffer:scroll_range(buffer.target_end, buffer.target_start)
+    end
+
+    ui.statusbar_text = buffer.selections .. " occurences"
+
+    return true
+  end
+
+  return false
+end
+
+M.select_all_words = function()
+  while M.select_word(true) == true do end
+
+end
+
+M.enclose = function(left, right)
+  for i = 0, buffer.selections - 1, 1 do
+    local s, e = buffer.selection_n_start[i], buffer.selection_n_end[i]
+
+    buffer:set_target_range(s,e)
+
+    local txt = left..buffer.target_text..right
+    buffer:replace_target(txt)
+--    buffer.selection_n_start[i] = buffer.selection_n_start[i] - #left
+    if s ~= e then
+      buffer.selection_n_end[i] = buffer:position_relative(buffer.selection_n_start[i], #txt)
+    else
+      buffer.selection_n_caret[i] = get_next_pos(buffer.selection_n_caret[i])
+      buffer.selection_n_anchor[i] = buffer.selection_n_caret[i]
+    end
   end
 end
 
--- Cancel selections on `Esc` key press.
-events.connect('keypress',
-  function(code, shift, control, alt)
-    if code == 0xff1b then -- Escape key
-      local prev_pos = buffer.current_pos
-      buffer:clear_selections()
-      buffer:set_selection(prev_pos, prev_pos)
-      positions = {}
-    elseif restore == true then
-      set_cursor_positions()
-      if code == 0xff08 or code == 0xff9f or code == 0xffff then
-        restore = false
-        positions = {}
-      end
-    end
-  end
-)
+M.unenclose = function(left, right)
+  -- move along, nothing to see here yet.
+end
 
-events.connect('char_added',
-  function()
-    restore = false
-    positions = {}
-    return
+M.char_right = function()
+  if buffer.selections < 2 then return false end
+  for i = 0, buffer.selections - 1, 1 do
+    buffer.selection_n_start[i] = get_next_pos(buffer.selection_n_end[i])
+    buffer.selection_n_end[i] = buffer.selection_n_end[i]
   end
-)
+  return true
+end
+M.char_right_extend = function()
+  if buffer.selections < 2 then return false end
+  for i = 0, buffer.selections - 1, 1 do
+    buffer.selection_n_caret[i] = get_next_pos(buffer.selection_n_caret[i])
+  end
+  return true
+end
+M.word_right = function()
+  if buffer.selections < 2 then return false end
+
+  for i = 0, buffer.selections - 1, 1 do
+    local nupos = buffer:word_end_position(buffer.selection_n_start[i], true)
+    if nupos == buffer.selection_n_start[i] then
+      nupos = get_next_pos(nupos)
+    end
+    buffer.selection_n_start[i] = nupos
+    buffer.selection_n_end[i] = nupos
+  end
+  return true
+end
+M.word_right_extend = function()
+  if buffer.selections < 2 then return false end
+  for i = 0, buffer.selections - 1, 1 do
+    local nupos = buffer:word_end_position(buffer.selection_n_caret[i], true)
+    if nupos == buffer.selection_n_caret[i] then
+      nupos = get_next_pos(nupos)
+    end
+    buffer.selection_n_caret[i] = nupos
+  end
+  return true
+end
+M.char_left = function()
+  if buffer.selections < 2 then return false end
+  for i = 0, buffer.selections - 1, 1 do
+    buffer.selection_n_start[i] = get_prev_pos(buffer.selection_n_start[i])
+    buffer.selection_n_end[i] = buffer.selection_n_start[i]
+  end
+  return true
+end
+M.char_left_extend = function()
+  if buffer.selections < 2 then return false end
+  for i = 0, buffer.selections - 1, 1 do
+    buffer.selection_n_caret[i] = get_prev_pos(buffer.selection_n_caret[i])
+  end
+  return true
+end
+M.word_left = function()
+  if buffer.selections < 2 then return false end
+
+  for i = 0, buffer.selections - 1, 1 do
+    local nupos = buffer:word_start_position(buffer.selection_n_start[i], true)
+    if nupos == buffer.selection_n_start[i] then
+      nupos = get_prev_pos(nupos)
+    end
+    buffer.selection_n_start[i] = nupos
+    buffer.selection_n_end[i] = nupos
+  end
+  return true
+end
+M.word_left_extend = function()
+  if buffer.selections < 2 then return false end
+  for i = 0, buffer.selections - 1, 1 do
+    local nupos = buffer:word_start_position(buffer.selection_n_caret[i], true)
+    if nupos == buffer.selection_n_caret[i] then
+      nupos = get_prev_pos(nupos)
+    end
+    buffer.selection_n_caret[i] = nupos
+  end
+  return true
+end
+M.line_down = function()
+  if buffer.selections < 2 then return false end
+end
+M.line_down_extend = function()
+  if buffer.selections < 2 then return false end
+end
+M.line_up = function()
+  if buffer.selections < 2 then return false end
+end
+M.line_up_extend = function()
+  if buffer.selections < 2 then return false end
+end
+M.line_start = function()
+  if buffer.selections < 2 then return false end
+  for i = 0, buffer.selections - 1, 1 do
+    local nupos = buffer:position_from_line(buffer:line_from_position(buffer.selection_n_caret[i]))
+    buffer.selection_n_start[i] = nupos
+    buffer.selection_n_end[i] = nupos
+  end
+  return true
+end
+M.line_start_extend = function()
+  if buffer.selections < 2 then return false end
+  for i = 0, buffer.selections - 1, 1 do
+    local nupos = buffer:position_from_line(buffer:line_from_position(buffer.selection_n_caret[i]))
+    buffer.selection_n_caret[i] = nupos
+  end
+  return true
+end
+M.line_end = function()
+  if buffer.selections < 2 then return false end
+  for i = 0, buffer.selections - 1, 1 do
+    local nupos = buffer.line_end_position[buffer:line_from_position(buffer.selection_n_caret[i])]
+    buffer.selection_n_start[i] = nupos
+    buffer.selection_n_end[i] = nupos
+  end
+  return true
+end
+M.line_end_extend = function()
+  if buffer.selections < 2 then return false end
+  for i = 0, buffer.selections - 1, 1 do
+    local nupos = buffer.line_end_position[buffer:line_from_position(buffer.selection_n_caret[i])]
+    buffer.selection_n_caret[i] = nupos
+  end
+  return true
+end
+M.page_up_extend = function()
+  if buffer.selections < 2 then return false end
+end
+M.page_down_extend = function()
+  if buffer.selections < 2 then return false end
+end
+
+
+M.newline = function()
+  if buffer.selections < 2 then return false end
+  for i = 0, buffer.selections - 1, 1 do
+    buffer:set_target_range(buffer.selection_n_start[i], buffer.selection_n_end[i])
+    buffer:replace_target("\n")
+    buffer.selection_n_start[i] = buffer:position_relative(buffer.selection_n_end[i], 1)
+    buffer.selection_n_end[i] = buffer.selection_n_end[i]
+  end
+  return true
+end
+
+
+M.prevent_crap = function()
+  if buffer.selections > 2 then return true end
+end
+
 
 return M
